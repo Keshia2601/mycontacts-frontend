5488f797fb7c1358555b9f3be2aae1cec53cfc7e
Keshia Adrin <keshady26.bae@gmail.com>
2026-02-07 11:18:52 +0530

    Snackbar and buttons

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,17 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import { AuthProvider } from "./auth/AuthContext.jsx";
+import Snackbar from "./components/Snackbar.jsx";
+import { NotificationProvider } from "./context/NotificationContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <NotificationProvider>
+          <App />
+          <Snackbar />
+        </NotificationProvider>
       </AuthProvider>
     </BrowserRouter>
   </StrictMode>
